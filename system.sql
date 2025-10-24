a736566e1b4ba04406386fed56d87c05aa6f8443
ParkJoon <pj0585@naver.com>
2025-10-24 12:09:50 +0900

    database sql 커밋

---
+++ system.sql
@@ -0,0 +1,9 @@
+-- c##을 사용안하도록 설정
+-- 오라클 버전이 업데이트 되면서 사용자 설정할 때 아이디 앞에 c##을 붙이도록 설정되어 있음
+-- hr 사용자 생성 => c##hr
+ALTER SESSION SET "_oracle_script"=TRUE;
+
+@C:\database\db-sample-schemas-main\human_resources\hr_install.SQL
+@C:\app\soldesk\product\21c\dbhomeXE\rdbms\admin\scott.SQL
+
+sys AS sysdba
