0db6760a8a17fae17ae6c6be0e25570a290a06c0
ParkJoon <pj0585@naver.com>
2025-10-30 18:18:50 +0900

    제약조건 학습 및 ERD 추가

--- root.sql
+++ root.sql
@@ -64,6 +64,13 @@ create database if not exists EXAM;
 
 use EXAM;
 -- 테이블 생성
+-- 데이터 유형
+-- 숫자형 : INT, TINYINT(1 byte), SMALLINT(2), MEDIUMINT(3), BIGINT(8)
+-- 실수형 : 1. 고정소수점 방식 (DECIMAL / NUMERIC)
+--		  2. 부동소수점 방식 (FLOAT, DOUBLE)
+-- 문자형 : CHAR(n) - 고정길이문자열
+-- 		  VARCHAR(n)
+-- 날짜형 : TIME / DATE / DATETIME
 create table TABLE1(
 	COL1 INT,
 	COL2 VARCHAR(50),
@@ -81,3 +88,70 @@ insert
 	TABLE2(COL2, COL3)
 values('TEST', '2025-10-29'); 
 select * from table2;
+
+insert
+	into
+	TABLE2(COL1, COL2, COL3)
+values(3, 'TEST', '2025-10-29'); 
+select * from table2;
+
+-- 현재 auto_increment로 생성된 마지막 값 확인
+select LAST_INSERT_ID();
+
+-- auto_increment 시작값 변경
+alter table table2 auto_increment = 100;
+
+-- auto_increment 증가값 변경
+set @@AUTO_INCREMENT_INCREMENT = 1;
+
+create table EXAM_INSERT_SELECT_FROM(
+	COL1 INT,
+	COL2 VARCHAR(10)
+);
+
+create table EXAM_INSERT_SELECT_TO(
+	COL1 INT,
+	COL2 VARCHAR(10)
+);
+
+insert into EXAM_INSERT_SELECT_FROM(COL1, COL2) values(1, 'Do');
+insert into EXAM_INSERT_SELECT_FROM(COL1, COL2) values(2, 'It');
+insert into EXAM_INSERT_SELECT_FROM(COL1, COL2) values(3, 'MySQL');
+select * from EXAM_INSERT_SELECT_FROM;
+
+-- EXAM_INSERT_SELECT_FROM => EXAM_INSERT_SELECT_TO
+insert into EXAM_INSERT_SELECT_TO as select * from EXAM_INSERT_SELECT_FROM;
+select * from EXAM_INSERT_SELECT_TO;
+
+create table EXAM_SELECT_NEW select * from EXAM_INSERT_SELECT_FROM;
+select * from EXAM_SELECT_NEW;
+
+create table EXAM_DATE_TABLE(
+	COL1 DATE, COL2 TIME, COL3 DATETIME, COL4 TIMESTAMP
+);
+insert into EXAM_DATE_TABLE values(now(),now(),now(),now());
+select * from EXAM_DATE_TABLE;
+
+-- 사용자 생성
+-- ID 대소문자 구별
+-- localhost : 내컴퓨터(로컬 접속만 가능)
+-- '%' : 모든 IP에서 접속 가능(외부 접속 허용)
+
+create user 'TEST1'@'localhost' IDENTIFIED by '12345';
+-- create user 'TEST1'@'%' IDENTIFIED by 12345;
+
+-- 권한 부여
+
+-- grant 권한목록 on 데이터베이스.테이블 to '사용자이름'@'호스트';
+-- grant SELCT, INSERT, update, ON exam.table1 to 'TEST1'@'localhost';
+
+-- 모든 권한 부여
+grant all privileges on exam.* to 'TEST1'@'localhost';
+-- 변경사항 반영
+flush privileges;
+
+-- 사용자 삭제
+drop user 'TEST1'@'localhost';
+
+-- 비밀번호 변경
+ALTER USER 'TEST1'@'localhost' IDENTIFIED BY '12345';
--- system.sql
+++ system.sql
@@ -11,3 +11,47 @@ ALTER SESSION SET "_oracle_script"=TRUE;
 GRANT CREATE VIEW TO SCOTT;
 GRANT CREATE SYNONYM TO SCOTT;
 GRANT CREATE PUBLIC SYNONYM TO SCOTT;
+
+-- 사용자
+-- DB에 접속하여 데이터 관리하는 계정
+
+-- 오라클 DB
+-- 테이블, 뷰, 인덱스, 시퀀스... => 업무별로 사용자 생성 후 객체 생성할 수 있는 권한 부여
+
+-- CREATE USER 사용자이름 IDENTIFIED BY 비밀번호;
+-- C## 접두어가 무조건 필요
+--CREATE USER TEST1 IDENTIFIED BY 12345;
+CREATE USER C##TEST1 IDENTIFIED BY 12345;
+
+-- C## 안 붙이려면 같이 실행
+ALTER SESSION SET "_oracle_script"=TRUE;
+CREATE USER TEST2 IDENTIFIED BY 12345;
+
+-- 사용자 TEST2는 CREATE SESSSION 권한을 가지고 있지 않음. 로그온 거절
+GRANT CREATE SESSION TO TEST2;
+
+-- 개별권한 묶어서 관리 => 롤
+GRANT CONNECT, RESOURCE TO TEST2;
+
+-- 테이블 스페이스 'USERS'에 대한 권한
+ALTER USER TEST2 DEFAULT TABLESPACE USERS QUOTA 2M ON USERS;
+
+-- 사용자 권한 + 생성
+-- 대소문자 구별안함 / 비번은 구별함
+CREATE USER TEST3 IDENTIFIED BY 12345 
+DEFAULT TABLESPACE USERS 
+TEMPORARY TABLESPACE TEMP
+QUOTA 2M ON USERS;
+
+-- 대부분의 권한
+GRANT CONNECT, RESOURCE TO TEST3;
+
+-- 사용자 삭제
+DROP USER test3 CASCADE;
+
+-- 권한 취소
+-- REVOKE 취소할권한이름 FROM 사용자명
+REVOKE CREATE SESSION FROM test2;
+
+-- 비밀번호 변경
+-- ALTER USER 사용자 IDENTIFIED BY 변경할비번
